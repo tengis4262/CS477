a0faf88ddd9ab303dbfa876447865081aae5fa9e
tengis4262 <95094921+tengis4262@users.noreply.github.com>
2023-07-27 14:12:57 -0500

    Update q1app.js

--- Day2/Assignment/q1app.js
+++ Day2/Assignment/q1app.js
@@ -2,3 +2,25 @@ let array = require('./q1')
 
 array.odd(test.arr);
 array.even(test.arr);
+
+
+// Homework Solutiion
+Array.prototype.even = function(){
+    const res = this.filter(num=> num%2 ==0);
+    console.log(res);
+};
+
+// const arr = [1,2,3,4,5,6,7,8]
+// arr.even();
+[1,2,3,4,5,6,7,8].even();
+
+Array.prototype.odd = function(){
+    const res = this.filter(num=> num%2 !=0);
+    console.log(res);
+};
+
+[1,2,3,4,5,6,7,8].odd();
+
+
+
+
