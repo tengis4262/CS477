1b5b25a3b04b91c2bb1315b58d390f18c4bd791c
tengis4262 <95094921+tengis4262@users.noreply.github.com>
2023-07-31 11:15:07 -0500

    Update q1.js

--- Day4/Assignment/q1.js
+++ Day4/Assignment/q1.js
@@ -3,6 +3,6 @@ const http = require('http');
 const port = 3000
 http.createServer((req,res)=>{
     res.end('Hello WORLD!!!')
-}).listen(3000,()=>{
+}).listen(port,()=>{
     console.log(`Server running on http://localhost:${port}`)
 })
