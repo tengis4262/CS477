2f7df3308732e6bd1831525c6404957d1ea21008
tengis4262 <95094921+tengis4262@users.noreply.github.com>
2023-07-31 14:42:09 -0500

    Delete zipUnzip.js

--- Day3/Assignment/q3/zipUnzip.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const zlib = require('zlib');
-const path = require('path');
-
-
-fs.createReadStream(path.join(__dirname, 'destination.txt.gz'))
-    .pipe(zlib.createGunzip())
-    .pipe(fs.createWriteStream(path.join(__dirname, 'abc.txt')))
